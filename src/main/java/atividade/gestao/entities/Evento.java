40b6e97be9e41096ada6d060bd6df5fbf1e5183e
LuisCantieri <luis.cantieri2510@gmail.com>
2025-05-26 15:34:19 -0300

    feat: entidades

---
+++ src/main/java/atividade/gestao/entities/Evento.java
@@ -0,0 +1,101 @@
+package atividade.gestao.entities;
+
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+	@Entity
+	@Table(name = "tb_evento")
+	public class Evento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	private Date data;
+	private String local;
+	private Integer vagas;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_evento_participante",
+	           joinColumns = @JoinColumn(name = "evento_id"),
+	           inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
+
+	
+	public Evento() {
+		
+	}
+
+	public Evento(Long id, String nome, String descricao, Date data, String local, Integer vagas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.data = data;
+		this.local = local;
+		this.vagas = vagas;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public Integer getVagas() {
+		return vagas;
+	}
+
+	public void setVagas(Integer vagas) {
+		this.vagas = vagas;
+	}
+	
+	
+}
---
+++ src/main/java/atividade/gestao/entities/Participante.java
@@ -0,0 +1,73 @@
+package atividade.gestao.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Column(columnDefinition = "TEXT")
+	private String email;
+	private Integer telefone;
+	
+	@ManyToMany(mappedBy = "participantes")
+    private Set<Evento> eventos = new HashSet<>();
+	
+	public Participante() {
+		
+	}
+
+	public Participante(Long id, String nome, String email, Integer telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+	
+}
